cc5e4b2875ab7b5341e0d45c9e15acb26d7f3d6c
firstRepo <hassanjawedwani@gmail.com>
2024-06-17 04:32:53 +0500

    checkpoint

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -45,37 +45,79 @@ import { useEffect, useState } from "react";
 // Get Request 
 
 const Api = () => {
-    const [data, setData] = useState([])
-    useEffect(()=>{
-        fetch("http://localhost:3000/posts")
-        .then(resp => {
-            resp.json().then(result =>{
-                setData(result);
-            });
-        });
-    }, []);
-    return (
-        <table border={1} style={{borderCollapse: "collapse"}}>
-            <thead>
-                <tr>
-                    <th>_Id</th>
-                    <th>Name</th>
-                    <th>Text</th>
+    const [data, setData] = useState([]);
+    
+    const [formData, setFormData]  = useState({ 
+        user: '',
+        views: 0
+    });
 
-                </tr>
-            </thead>
-            <tbody>
-            {
-                data.map((obj, index) => (
-                    <tr key={index}>
-                        <td>{obj.id}</td>
-                        <td>{obj.title}</td>
-                        <td>{obj.views}</td>
+    const updateApi = async() => {
+        await fetch("http://localhost:3000/posts", {
+            method: "Post",
+            headers: {
+                "Accept": "Application/json",
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        updateTable();
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        updateApi();
+    }
+
+    const inputHandler = (e) => {
+        const {name, value} = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+        console.log(formData);
+    }
+
+    const updateTable = async() => {
+        const result = await fetch("http://localhost:3000/posts").then(resp => resp.json().then(res => res));
+        setData(result);
+    }
+
+    useEffect(()=>{
+        updateTable();
+    },[]);
+
+    return (
+        <div>
+            <table border={1} style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th>_Id</th>
+                        <th>Name</th>
+                        <th>Text</th>
+
                     </tr>
-                ))
-                }
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                {
+                    data.map((obj, index) => (
+                        <tr key={index}>
+                            <td>{obj.id}</td>
+                            <td>{obj.user}</td>
+                            <td>{obj.views}</td>
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
+            <div>
+                <form onSubmit={submitHandler}>
+                    <input type="text" placeholder="Enter user:" name="user" onChange={inputHandler} required/>
+                    <input type="number" placeholder="Enter no of views: " min="0" name="views" onChange={inputHandler} required/>
+                    <button type="submit">Add Data in Apis</button>
+                </form>
+            </div>
+        </div>
     );
 }
 

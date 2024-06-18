e62641c378d6661d6697843056d474ca36fcac5b
firstRepo <hassanjawedwani@gmail.com>
2024-06-18 07:49:51 +0500

    checkpoint

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,7 +1,7 @@
-// import { useEffect, useState } from "react"; 
+// import { useEffect, useState } from "react";
 
 import { useEffect, useState } from "react";
-
+import ClipLoader from "react-spinners/ClipLoader";
 
 // const Api = () => {
 //     const [apiData, setApiData]  = useState([]);
@@ -41,84 +41,151 @@ import { useEffect, useState } from "react";
 
 // export default Api;
 
+// Get Request
 
-// Get Request 
+const initialState = {
+  user: "",
+  views: "",
+};
 
 const Api = () => {
-    const [data, setData] = useState([]);
-    
-    const [formData, setFormData]  = useState({ 
-        user: '',
-        views: 0
-    });
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState(initialState);
+  const [loading, setLoading] = useState(false);
 
-    const updateApi = async() => {
-        await fetch("http://localhost:3000/posts", {
-            method: "Post",
-            headers: {
-                "Accept": "Application/json",
-                "Content-Type": "Application/json"
-            },
-            body: JSON.stringify(formData)
-        })
-        updateTable();
+  const createPost = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/posts", {
+        method: "POST",
+        headers: {
+          Accept: "Application/json",
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("failed to create post, Error 201");
+      }
+      await updateTable();
+    } catch (error) {
+      console.error("Error occured: ", error);
     }
+  };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        updateApi();
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    await createPost();
+    setFormData(initialState);
+  };
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const updateTable = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/posts");
+      if (!response.ok) {
+        throw new Error("Failed to update table");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error occured: ", error);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
     }
+  };
 
-    const inputHandler = (e) => {
-        const {name, value} = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-        console.log(formData);
+  const deleteHandler = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete post");
+      }
+      await updateTable();
+    } catch (error) {
+      console.error("Error occured: ", error);
     }
+  };
 
-    const updateTable = async() => {
-        const result = await fetch("http://localhost:3000/posts").then(resp => resp.json().then(res => res));
-        setData(result);
-    }
+  useEffect(() => {
+    updateTable();
+  }, []);
 
-    useEffect(()=>{
-        updateTable();
-    },[]);
-
-    return (
-        <div>
-            <table border={1} style={{borderCollapse: "collapse"}}>
-                <thead>
-                    <tr>
-                        <th>_Id</th>
-                        <th>Name</th>
-                        <th>Text</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    data.map((obj, index) => (
-                        <tr key={index}>
-                            <td>{obj.id}</td>
-                            <td>{obj.user}</td>
-                            <td>{obj.views}</td>
-                        </tr>
-                    ))
-                    }
-                </tbody>
-            </table>
-            <div>
-                <form onSubmit={submitHandler}>
-                    <input type="text" placeholder="Enter user:" name="user" onChange={inputHandler} required/>
-                    <input type="number" placeholder="Enter no of views: " min="0" name="views" onChange={inputHandler} required/>
-                    <button type="submit">Add Data in Apis</button>
-                </form>
-            </div>
+  return (
+    <div>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ClipLoader color="#000" loading={loading} size={50} />
         </div>
-    );
-}
+      ) : (
+        <>
+          <table border={1} style={{ borderCollapse: "collapse" }}>
+            <thead>
+              <tr>
+                <th>_Id</th>
+                <th>Name</th>
+                <th>Text</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((obj, index) => (
+                <tr key={index}>
+                  <td>{obj.id}</td>
+                  <td>{obj.user}</td>
+                  <td>{obj.views}</td>
+                  <td>
+                    <button type="button" onClick={() => deleteHandler(obj.id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div>
+            <form onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Enter user:"
+                name="user"
+                value={formData.user}
+                onChange={inputHandler}
+                required
+              />
+              <input
+                type="number"
+                placeholder="Enter no of views: "
+                min="0"
+                name="views"
+                value={formData.views}
+                onChange={inputHandler}
+                required
+              />
+              <button type="submit">Add Data in Apis</button>
+            </form>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Api;
